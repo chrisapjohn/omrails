fd085329d0fd863995ed4362c910b214c8c464be
chrisapjohn <chrisupjohn@gmail.com>
2013-05-24 14:46:37 -0700

    Again

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -7,6 +7,9 @@ class Pin < ActiveRecord::Base
 																content_type: { content_type: ['image/jpeg','image/jpg','image/png','image/gif']},
 																size: { less_than: 5.megabytes }
   belongs_to :user
-  has_attached_file :image
+  has_attached_file :image,
+  	:styles => { :medium => "275x275>", :thumb => "175x155>" },
+    :storage => :s3, :s3_credentials => "#{Rails.root}/config/amazon_s3.yml",
+    :path => "user/:attachment/:style/:id.:extension"
   
 end
